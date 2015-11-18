08c93a9152f1e854dc07862d6ce71263aac54dca
Marcos <Marcos@MarcosPC>
2015-11-18 21:29:36 -0200

    Criação da primeira fase

--- Jogo/src/br/jogo/listeners/StarFrameListener.java
+++ Jogo/src/br/jogo/listeners/StarFrameListener.java
@@ -7,19 +7,25 @@ package br.jogo.listeners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import telas.StartFrame;
 
 /**
  *
  * @author Marcos
  */
 public class StarFrameListener implements ActionListener {
+    StartFrame frame;
+    
+        public StarFrameListener(StartFrame frame) {
+        this.frame = frame;
+    }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         if (null != ae.getActionCommand()) {
 
             switch (ae.getActionCommand()) {
-                
+
                 
             }
 
