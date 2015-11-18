27b47ec0816a9f9da3b243c47b6f57f50ee03643
Marcos <Marcos@MarcosPC>
2015-11-18 20:47:21 -0200

    Criação dos eventos do StartFrameListener

---
+++ Jogo/src/br/jogo/listeners/StarFrameListener.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.jogo.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Marcos
+ */
+public class StarFrameListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (null != ae.getActionCommand()) {
+
+            switch (ae.getActionCommand()) {
+                
+                
+            }
+
+        }
+    }
+
+}
